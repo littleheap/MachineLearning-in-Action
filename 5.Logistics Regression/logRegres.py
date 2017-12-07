c1131e2a5744803cade13f8055676518ec2c2f7c
LittleHeap <418761605@qq.com>
2017-12-07 21:02:47 +0800

    update

--- 5.Logistics Regression/logRegres.py
+++ 5.Logistics Regression/logRegres.py
@@ -1,113 +1,165 @@
-'''
-Created on Oct 27, 2010
-Logistic Regression Working Module
-@author: Peter
-'''
 from numpy import *
 
+
+# 加载数据集
 def loadDataSet():
-    dataMat = []; labelMat = []
+    dataMat = [];
+    labelMat = []
     fr = open('testSet.txt')
     for line in fr.readlines():
         lineArr = line.strip().split()
+        # 每个点设置三个参量特征，第一个默认为1，后面两个为坐标值
         dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        # 建立label矩阵
         labelMat.append(int(lineArr[2]))
-    return dataMat,labelMat
+    return dataMat, labelMat
 
+
+# Sigmoid函数
 def sigmoid(inX):
-    return 1.0/(1+exp(-inX))
+    return 1.0 / (1 + exp(-inX))
 
+
+# 梯度上升训练函数
 def gradAscent(dataMatIn, classLabels):
-    dataMatrix = mat(dataMatIn)             #convert to NumPy matrix
-    labelMat = mat(classLabels).transpose() #convert to NumPy matrix
-    m,n = shape(dataMatrix)
+    # 转换成矩阵格式
+    dataMatrix = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    # 获取矩阵尺寸
+    m, n = shape(dataMatrix)
+    # 设置学习率
     alpha = 0.001
+    # 设置递归迭代次数
     maxCycles = 500
-    weights = ones((n,1))
-    for k in range(maxCycles):              #heavy on matrix operations
-        h = sigmoid(dataMatrix*weights)     #matrix mult
-        error = (labelMat - h)              #vector subtraction
-        weights = weights + alpha * dataMatrix.transpose()* error #matrix mult
+    # 初始化权值矩阵
+    weights = ones((n, 1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix * weights)
+        error = (labelMat - h)
+        weights = weights + alpha * dataMatrix.transpose() * error
     return weights
 
+
+dataArr, labelMat = loadDataSet()
+weights = gradAscent(dataArr, labelMat)
+print(weights)
+
+
+# 绘制决策边界
 def plotBestFit(weights):
     import matplotlib.pyplot as plt
-    dataMat,labelMat=loadDataSet()
+    dataMat, labelMat = loadDataSet()
     dataArr = array(dataMat)
-    n = shape(dataArr)[0] 
-    xcord1 = []; ycord1 = []
-    xcord2 = []; ycord2 = []
+    n = shape(dataArr)[0]
+    xcord1 = []
+    ycord1 = []
+    xcord2 = []
+    ycord2 = []
     for i in range(n):
-        if int(labelMat[i])== 1:
-            xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i, 1])
+            ycord1.append(dataArr[i, 2])
         else:
-            xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])
+            xcord2.append(dataArr[i, 1])
+            ycord2.append(dataArr[i, 2])
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
     ax.scatter(xcord2, ycord2, s=30, c='green')
     x = arange(-3.0, 3.0, 0.1)
-    y = (-weights[0]-weights[1]*x)/weights[2]
+    y = (-weights[0] - weights[1] * x) / weights[2]
     ax.plot(x, y)
-    plt.xlabel('X1'); plt.ylabel('X2');
+    plt.xlabel('X1')
+    plt.ylabel('X2')
     plt.show()
 
+
+plotBestFit(weights.getA())
+
+
+# 随机梯度上升函数
 def stocGradAscent0(dataMatrix, classLabels):
-    m,n = shape(dataMatrix)
+    m, n = shape(dataMatrix)
     alpha = 0.01
-    weights = ones(n)   #initialize to all ones
+    weights = ones(n)
     for i in range(m):
-        h = sigmoid(sum(dataMatrix[i]*weights))
+        h = sigmoid(sum(dataMatrix[i] * weights))
         error = classLabels[i] - h
         weights = weights + alpha * error * dataMatrix[i]
     return weights
 
+
+dataArr, labelMat = loadDataSet()
+weights = stocGradAscent0(array(dataArr), labelMat)
+print(weights)
+
+
+# 改进的随机梯度上升函数
 def stocGradAscent1(dataMatrix, classLabels, numIter=150):
-    m,n = shape(dataMatrix)
-    weights = ones(n)   #initialize to all ones
+    m, n = shape(dataMatrix)
+    weights = ones(n)
     for j in range(numIter):
-        dataIndex = range(m)
+        dataIndex = list(range(m))
         for i in range(m):
-            alpha = 4/(1.0+j+i)+0.0001    #apha decreases with iteration, does not 
-            randIndex = int(random.uniform(0,len(dataIndex)))#go to 0 because of the constant
-            h = sigmoid(sum(dataMatrix[randIndex]*weights))
+            # 学习率主键降低
+            alpha = 4 / (1.0 + j + i) + 0.0001
+            randIndex = int(random.uniform(0, len(dataIndex)))
+            h = sigmoid(sum(dataMatrix[randIndex] * weights))
             error = classLabels[randIndex] - h
             weights = weights + alpha * error * dataMatrix[randIndex]
-            del(dataIndex[randIndex])
+            del (dataIndex[randIndex])
     return weights
 
+
+dataArr, labelMat = loadDataSet()
+weights = stocGradAscent1(array(dataArr), labelMat)
+print(weights)
+plotBestFit(weights)
+
+
+# 基于Sigmoid函数的分类函数
 def classifyVector(inX, weights):
-    prob = sigmoid(sum(inX*weights))
-    if prob > 0.5: return 1.0
-    else: return 0.0
+    prob = sigmoid(sum(inX * weights))
+    if prob > 0.5:
+        return 1.0
+    else:
+        return 0.0
+
 
 def colicTest():
-    frTrain = open('horseColicTraining.txt'); frTest = open('horseColicTest.txt')
-    trainingSet = []; trainingLabels = []
+    frTrain = open('horseColicTraining.txt');
+    frTest = open('horseColicTest.txt')
+    trainingSet = [];
+    trainingLabels = []
     for line in frTrain.readlines():
         currLine = line.strip().split('\t')
-        lineArr =[]
+        lineArr = []
         for i in range(21):
             lineArr.append(float(currLine[i]))
         trainingSet.append(lineArr)
         trainingLabels.append(float(currLine[21]))
     trainWeights = stocGradAscent1(array(trainingSet), trainingLabels, 1000)
-    errorCount = 0; numTestVec = 0.0
+    errorCount = 0;
+    numTestVec = 0.0
     for line in frTest.readlines():
         numTestVec += 1.0
         currLine = line.strip().split('\t')
-        lineArr =[]
+        lineArr = []
         for i in range(21):
             lineArr.append(float(currLine[i]))
-        if int(classifyVector(array(lineArr), trainWeights))!= int(currLine[21]):
+        if int(classifyVector(array(lineArr), trainWeights)) != int(currLine[21]):
             errorCount += 1
-    errorRate = (float(errorCount)/numTestVec)
-    print "the error rate of this test is: %f" % errorRate
+    errorRate = (float(errorCount) / numTestVec)
+    print("the error rate of this test is: %f" % errorRate)
     return errorRate
 
+
 def multiTest():
-    numTests = 10; errorSum=0.0
+    numTests = 10;
+    errorSum = 0.0
     for k in range(numTests):
         errorSum += colicTest()
-    print "after %d iterations the average error rate is: %f" % (numTests, errorSum/float(numTests))
-        
+    print("after %d iterations the average error rate is: %f" % (numTests, errorSum / float(numTests)))
+
+
+multiTest()
